2d0407c60897f211d32aac44d4fcad12b87e8f24
Chris Price <chris@puppetlabs.com>
2014-12-09 14:16:58 -0800

    Empty project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hsql-scratch
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/hsql_scratch/core_test.clj
@@ -0,0 +1,7 @@
+(ns hsql-scratch.core-test
+  (:require [clojure.test :refer :all]
+            [hsql-scratch.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
